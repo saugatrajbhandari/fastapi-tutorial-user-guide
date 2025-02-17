49b9c7adb87e0348baa553b823fa98f1608eba60
saugat rajbhandari <saugatrajbhandari47@gmail.com>
2025-02-17 18:18:10 +0545

    update

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from enum import Enum
+from pydantic import BaseModel
+from typing import Annotated
+
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
 app = FastAPI()
@@ -15,8 +25,16 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(item_id: int, needy:str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
@@ -28,3 +46,18 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}    
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_items(q: Annotated[str | None, Query(max_length=3)] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
